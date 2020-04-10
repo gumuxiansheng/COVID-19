b1fbcde9c9a9ee1a07afdc57ccd253954f789217
Mike Zhu <zhuyuan2706@gmail.com>
2020-04-10 15:24:44 +0800

    refine

---
+++ src/cvrp.cc
@@ -0,0 +1,114 @@
+#include <vector>
+
+#include "ortools/constraint_solver/routing.h"
+#include "ortools/constraint_solver/routing_enums.pb.h"
+#include "ortools/constraint_solver/routing_index_manager.h"
+#include "ortools/constraint_solver/routing_parameters.h"
+
+namespace operations_research
+{
+struct DataModel
+{
+    const std::vector<std::vector<int64>> distance_matrix{};
+    const std::vector<int64> demands{};
+    const std::vector<int64> vehicle_capacities{15, 15, 15, 15};
+    const int num_vehicles = 4;
+    const RoutingIndexManager::NodeIndex depot{0};
+};
+
+//! @brief Print the solution.
+//! @param[in] data Data of the problem.
+//! @param[in] manager Index manager used.
+//! @param[in] routing Routing solver used.
+//! @param[in] solution Solution found by the solver.
+void PrintSolution(const DataModel &data, const RoutingIndexManager &manager,
+                   const RoutingModel &routing, const Assignment &solution)
+{
+    int64 total_distance{0};
+    int64 total_load{0};
+    for (int vehicle_id = 0; vehicle_id < data.num_vehicles; ++vehicle_id)
+    {
+        int64 index = routing.Start(vehicle_id);
+        LOG(INFO) << "Route for Vehicle " << vehicle_id << ":";
+        int64 route_distance{0};
+        int64 route_load{0};
+        std::stringstream route;
+        while (routing.IsEnd(index) == false)
+        {
+            int64 node_index = manager.IndexToNode(index).value();
+            route_load += data.demands[node_index];
+            route << node_index << " Load(" << route_load << ") -> ";
+            int64 previous_index = index;
+            index = solution.Value(routing.NextVar(index));
+            route_distance += routing.GetArcCostForVehicle(previous_index, index,
+                                                           int64{vehicle_id});
+        }
+        LOG(INFO) << route.str() << manager.IndexToNode(index).value();
+        LOG(INFO) << "Distance of the route: " << route_distance << "m";
+        LOG(INFO) << "Load of the route: " << route_load;
+        total_distance += route_distance;
+        total_load += route_load;
+    }
+    LOG(INFO) << "Total distance of all routes: " << total_distance << "m";
+    LOG(INFO) << "Total load of all routes: " << total_load;
+    LOG(INFO) << "";
+    LOG(INFO) << "Advanced usage:";
+    LOG(INFO) << "Problem solved in " << routing.solver()->wall_time() << "ms";
+}
+
+void VrpCapacity()
+{
+    // Instantiate the data problem.
+    DataModel data;
+
+    // Create Routing Index Manager
+    RoutingIndexManager manager(data.distance_matrix.size(), data.num_vehicles,
+                                data.depot);
+
+    // Create Routing Model.
+    RoutingModel routing(manager);
+
+    // Create and register a transit callback.
+    const int transit_callback_index = routing.RegisterTransitCallback(
+        [&data, &manager](int64 from_index, int64 to_index) -> int64 {
+            // Convert from routing variable Index to distance matrix NodeIndex.
+            int from_node = manager.IndexToNode(from_index).value();
+            int to_node = manager.IndexToNode(to_index).value();
+            return data.distance_matrix[from_node][to_node];
+        });
+
+    // Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index);
+
+    // Add Capacity constraint.
+    const int demand_callback_index = routing.RegisterUnaryTransitCallback(
+        [&data, &manager](int64 from_index) -> int64 {
+            // Convert from routing variable Index to demand NodeIndex.
+            int from_node = manager.IndexToNode(from_index).value();
+            return data.demands[from_node];
+        });
+    routing.AddDimensionWithVehicleCapacity(
+        demand_callback_index,   // transit callback index
+        int64{0},                // null capacity slack
+        data.vehicle_capacities, // vehicle maximum capacities
+        true,                    // start cumul to zero
+        "Capacity");
+
+    // Setting first solution heuristic.
+    RoutingSearchParameters searchParameters = DefaultRoutingSearchParameters();
+    searchParameters.set_first_solution_strategy(
+        FirstSolutionStrategy::PATH_CHEAPEST_ARC);
+
+    // Solve the problem.
+    const Assignment *solution = routing.SolveWithParameters(searchParameters);
+
+    // Print solution on console.
+    PrintSolution(data, manager, routing, *solution);
+}
+} // namespace operations_research
+
+int main(int argc, char **argv)
+{
+    operations_research::VrpCapacity();
+    return EXIT_SUCCESS;
+}
--- src/hello.cc
+++ src/hello.cc
@@ -1,54 +1,24 @@
 #include <iostream>
 #include <string>
 
-#include "ortools/linear_solver/linear_solver.h"
-#include "utils/file_util.h"
-
-namespace operations_research
+namespace covid19
 {
 void run()
 {
-  std::cout << "Hello" << std::endl;
-  // Create the linear solver with the GLOP backend.
-  MPSolver solver("This is a demo", MPSolver::GLOP_LINEAR_PROGRAMMING);
+  std::cout << "Hello, here you can choose what to run:" << std::endl;
+  std::cout << "[vrp|cvrp]" << std::endl;
+  char program[20];
+  std::cin >> program;
 
-  // Create the variables x and y.
-  MPVariable *const x = solver.MakeNumVar(0.0, 1, "x");
-  MPVariable *const y = solver.MakeNumVar(0.0, 2, "y");
-
-  LOG(INFO) << "Number of variables = " << solver.NumVariables();
-
-  // Create a linear constraint, 0 <= x + y <= 2.
-  MPConstraint *const ct = solver.MakeRowConstraint(0.0, 2.0, "ct");
-  ct->SetCoefficient(x, 1);
-  ct->SetCoefficient(y, 1);
-
-  LOG(INFO) << "Number of constraints = " << solver.NumConstraints();
-
-  // Create the objective function, 3 * x + y.
-  MPObjective *const objective = solver.MutableObjective();
-  objective->SetCoefficient(x, 3);
-  objective->SetCoefficient(y, 1);
-  objective->SetMaximization();
-
-  solver.Solve();
-
-  LOG(INFO) << "Solution:" << std::endl;
-  LOG(INFO) << "Objective value = " << objective->Value();
-  LOG(INFO) << "x = " << x->solution_value();
-  LOG(INFO) << "y = " << y->solution_value();
+  char pro_r[20];
+  strcpy(pro_r, "./");
+  strcat(pro_r, program);
+  std::system(pro_r);
 }
-} // namespace operations_research
+} // namespace covid19
 
 int main()
 {
-  operations_research::run();
-
-  // std::string file_url = "/Volumes/Mike_External/Dev/COVID-19/data/demo/Christofides_1_50.txt";
-  // // std::string file_url;
-  // // std::cout<<"please enter the file url:"<<std::endl;
-  // // std::cin>>file_url;
-
-  // covid19::ReadChristofides(file_url);
+  covid19::run();
   return EXIT_SUCCESS;
 }
