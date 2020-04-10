244419e01739fd370260683b82150806b69913fb
Mike Zhu <zhuyuan2706@gmail.com>
2020-04-11 01:03:28 +0800

    refine

--- src/hello.cc
+++ src/hello.cc
@@ -5,14 +5,16 @@ namespace covid19
 {
 void run()
 {
+  const char* folder_spl = "./";
   std::cout << "Hello, here you can choose what to run:" << std::endl;
   std::cout << "[vrp|cvrp]" << std::endl;
-  char program[20];
+  char program[10];
   std::cin >> program;
 
   char pro_r[20];
-  strcpy(pro_r, "./");
+  strcpy(pro_r, folder_spl);
   strcat(pro_r, program);
+  std::cout << pro_r;
   std::system(pro_r);
 }
 } // namespace covid19
